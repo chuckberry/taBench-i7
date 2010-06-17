9f233731c777fba3cc5889c5c9cf8bf95af62e1c
Lucas De Marchi <lucas.de.marchi@gmail.com>
2010-06-17 18:00:53 +0200

    Using 4KB samples by default
    This patch make use of 4KB samples instead of the really small 4B
    samples.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- const.h
+++ const.h
@@ -1,7 +1,7 @@
 #ifndef __CONST_IDT
 #define __CONST_IDT
 
-#define BUFLENWORD (2*1) /* betzw: must be a multiple of 2 because of stereo PCM audio data */
+#define BUFLENWORD (2*1024) /* betzw: must be a multiple of 2 because of stereo PCM audio data */
 #define BUFLENBYTE (2*BUFLENWORD)
 
 #ifndef _GNU_SOURCE
