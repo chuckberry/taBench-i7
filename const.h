aab4c81ac409f5bd5c742a90f896f3f027acde90
Lucas De Marchi <lucas.de.marchi@gmail.com>
2010-06-17 17:20:01 +0200

    Add support for X86_64 architectures
    Add the proper values for syscall definitions regarding x86 64bit
    machines.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- const.h
+++ const.h
@@ -5,7 +5,7 @@
 #define BUFLENBYTE (2*BUFLENWORD)
 
 #ifndef _GNU_SOURCE
-#define _GNU_SOURCE
+# define _GNU_SOURCE
 #endif
 #include <unistd.h>
 #include <sys/syscall.h>
@@ -13,12 +13,22 @@
 
 #define gettid() syscall(SYS_gettid)
 
-#define __NR_sched_add_taskaffinity 337
-#define sched_add_taskaffinity(tsk) \
+#ifdef __x86_64__
+# define __NR_sched_add_taskaffinity 299
+# define sched_add_taskaffinity(tsk) \
+       syscall(__NR_sched_add_taskaffinity, tsk)
+
+# define __NR_sched_del_taskaffinity 300
+# define sched_del_taskaffinity(tsk) \
+       syscall(__NR_sched_del_taskaffinity, tsk)
+#elif __i386__
+# define __NR_sched_add_taskaffinity 337
+# define sched_add_taskaffinity(tsk) \
 	syscall(__NR_sched_add_taskaffinity, tsk)
 
-#define __NR_sched_del_taskaffinity 338
-#define sched_del_taskaffinity(tsk) \
+# define __NR_sched_del_taskaffinity 338
+# define sched_del_taskaffinity(tsk) \
 	syscall(__NR_sched_del_taskaffinity, tsk)
+#endif
 
 #endif // __CONST_IDT
